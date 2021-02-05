ab01c41ac156bb05a6b5300a5613c4800dcaff14
David Gordon Ramsay <david.ramsay95@gmail.com>
2021-02-05 09:57:08 +0100

    Start table extractor implementation

--- run_tables.py
+++ run_tables.py
@@ -1,6 +1,13 @@
-from arxivtables.arxiv_getter.arxiv_getter import ArxivGetter
-#from arxivtables.table_extractor.table_extractor import TableExtractor
+#from arxivtables.arxiv_getter.arxiv_getter import ArxivGetter
+from arxivtables.table_extractor.table_extractor import TableExtractor
+import os
 
-aGetter = ArxivGetter()
 
-aGetter.getLatestPapers()
+DIR = os.getcwd()+'/tests/tables/'
+
+tables = sorted([DIR + filename for filename in os.listdir(
+    DIR) if os.path.isfile(os.path.join(DIR, filename))])
+
+for table in tables:
+    extractor = TableExtractor(table)
+    print(extractor.find_all_table_indices())
