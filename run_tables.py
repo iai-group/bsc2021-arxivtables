b9a2915f4ac2bd3b80dea4c61b3066565cb039e3
David Ramsay <david.ramsay95@gmail.com>
2021-04-11 20:57:10 +0200

    Argh added to parse CLI args

--- run_tables.py
+++ run_tables.py
@@ -3,64 +3,69 @@ __maintainer__ = 'Rebeca Pop, David Ramsay'
 
 import os
 import json
+import argh
 from datetime import datetime
 from pymongo import MongoClient
 from arxivtables.arxiv_getter.arxiv_getter import ArxivGetter
 from arxivtables.arxiv_watcher.arxiv_watcher import ArxivWatcher
 from arxivtables.table_extractor.table_extractor import TableExtractor
 from arxivtables.table_extractor.table_parser import TableParser
-import sys
 
-args = sys.argv[1:]
 
-client = MongoClient('localhost', 27017)
-db = client['db']
-collection_arxiv_papers = db['arxiv_papers']
+def set_up_mongo():
+    client = MongoClient('localhost', 27017)
+    db = client['db']
+    collection = db['arxiv_papers']
+    return client, db, collection
 
-client = MongoClient('localhost', 27017)
-db = client['db']
-collection_arxiv_papers = db['arxiv_papers']
 
-aw = ArxivWatcher()
+def run_tables(downloader_date=str(datetime.date(datetime.now())).replace('-', ''), mongo=False):
+    if mongo:
+        client, db, collection_arxiv_papers = set_up_mongo()
 
-if len(sys.argv) == 1:
-    paper_ids = aw.get_latest_papers(str(datetime.date(datetime.now())).replace('-', ''))
-else:
-    with open('logs/downloader/{}.log'.format(sys.argv[1])) as file:
-        paper_ids = [item[:-1] for item in file.readlines()]
+    aw = ArxivWatcher()
 
-for index, p_id in enumerate(paper_ids):
-    print('[Item {} of {}]'.format(index + 1, len(paper_ids)))
-    ag = ArxivGetter(p_id)
-    print(ag.paper_id)
-    ag.get_paper()
-    te = TableExtractor(p_id)
-    try:
-        te.extract_files()
-        tables = te.extract_tables()
-        if tables is None:
-            continue
-        tp = TableParser()
-        parsed = None
-        paper_dict = None
-        with open('db/arxiv_papers/' + p_id + '.json') as j:
-            paper_dict = json.load(j)
-        for table in tables:
-            parsed = tp.parse(table)
-            paper_dict["tables"].append(parsed.toJSON())
-        with open('db/arxiv_papers/' + p_id + '.json', 'w') as j:
-            json.dump(paper_dict, j, indent=2)
+    if downloader_date == str(datetime.date(datetime.now())).replace('-', ''):
+        aw.get_latest_papers()
 
-        collection_arxiv_papers.insert_one(paper_dict)
-        print(p_id + " inserted")
-        paper_dict = None
-    except Exception as e:
-        print(e)
-    ag.delete()
-client.close()
+    paper_ids = aw.get_ids_from_log(downloader_date)
 
-for jsonFile in os.listdir('db/arxiv_papers/'):
-    with open('db/arxiv_papers/' + jsonFile) as jf:
-        jf = json.load(jf)
-        if len(jf["tables"]) > 0:
-            print(jsonFile)
+    for index, p_id in enumerate(paper_ids):
+        print('[Item {} of {}]'.format(index + 1, len(paper_ids)))
+        ag = ArxivGetter(p_id)
+        print(ag.paper_id)
+        ag.get_paper()
+        te = TableExtractor(p_id)
+        try:
+            te.extract_files()
+            tables = te.extract_tables()
+            if tables is None:
+                continue
+            tp = TableParser()
+            parsed = None
+            paper_dict = None
+            with open('db/arxiv_papers/' + p_id + '.json') as j:
+                paper_dict = json.load(j)
+            for table in tables:
+                parsed = tp.parse(table)
+                paper_dict["tables"].append(parsed.toJSON())
+            with open('db/arxiv_papers/' + p_id + '.json', 'w') as j:
+                json.dump(paper_dict, j, indent=2)
+            collection_arxiv_papers.insert_one(paper_dict)
+            print(p_id + " inserted")
+            paper_dict = None
+        except Exception as e:
+            print(e)
+        ag.delete()
+    if mongo:
+        client.close()
+
+    for jsonFile in os.listdir('db/arxiv_papers/'):
+        with open('db/arxiv_papers/' + jsonFile) as jf:
+            jf = json.load(jf)
+            if len(jf["tables"]) > 0:
+                print(jsonFile)
+
+
+if __name__ == '__main__':
+    argh.dispatch_command(run_tables)
