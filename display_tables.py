b47da4d2f5bb95d8f42acf308aa0684ae0b0071b
Rebeca Pop <54622101+rebecom44@users.noreply.github.com>
2021-04-15 16:41:06 +0200

    Apply suggestions from code review
    Co-authored-by: David <2072677+davidramsay95@users.noreply.github.com>

--- display_tables.py
+++ display_tables.py
@@ -5,19 +5,17 @@ from tabulate import tabulate
 if __name__ == "__main__":
 
     paper_id = input("Please input the Arxiv paper ID: ")
-    f = None
 
     try:
-        f = open("db/arxiv_papers/" + paper_id + ".json", "r")
-        paper_dict = json.load(f)
-        f.close()
+       with open("db/arxiv_papers/" + paper_id + ".json", "r") as f:
+            paper_dict = json.load(f)
 
         tables = paper_dict["tables"]
         if not tables:
             print("There are not any tables in the specified paper.")
         else:
             try:
-                table_id = int(input("Please enter the table ID (0 to " + str(len(tables) - 1) + "): "))
+                table_id = int(input("Please enter the table ID (0 - " + str(len(tables) - 1) + "): "))
                 table = tables[table_id]
 
                 if "caption" in table and table["caption"]:
@@ -29,7 +27,5 @@ if __name__ == "__main__":
                 print("Please enter a valid table ID.")
 
     except FileNotFoundError:
-        print("The requested paper id was not found in our database.")
-    finally:
-        if f and not f.closed:
+        print("The requested paper ID was not found in the database.")
             f.close()
