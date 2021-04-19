f6dd48b427d58d13560bbbaf789b06b2de4aec1b
David Ramsay <david.ramsay95@gmail.com>
2021-04-19 22:02:15 +0200

    Fix install.sh 2

--- install.sh
+++ install.sh
@@ -3,7 +3,8 @@
 THISDIR=$(echo `pwd`)
 
 crontab -l | \
-{ cat; \
-echo "0 6 * * * docker-compose -f $THISDIR/docker-compose.yml up -d --build"; } \
+{ cat;
+echo \
+"0 6 * * * docker-compose -f $THISDIR/docker-compose.yml up -d --build"; } \
 | crontab -
 
