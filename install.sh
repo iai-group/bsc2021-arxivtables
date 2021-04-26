58fb2bb2e694642762d4ecd0839a2950d849d299
David Ramsay <david.ramsay95@gmail.com>
2021-04-26 09:31:20 +0200

    Typo in install.sh

--- install.sh
+++ install.sh
@@ -7,5 +7,5 @@ DCDIR=$(which docker-compose)
 crontab -l | \
 { cat;
 echo \
-"0 6 * * * $DC -f $THISDIR/docker-compose.yml up -d"; } \
+"0 6 * * * $DCDIR -f $THISDIR/docker-compose.yml up -d"; } \
 | crontab -
